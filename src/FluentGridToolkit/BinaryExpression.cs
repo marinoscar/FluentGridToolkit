7e1b44cc6be9305d040dda8a44188735c0106f74
Oscar Marin <oscar@marin.cr>
2024-12-07 22:25:52 -0600

    added a new way to filter the information

---
+++ src/FluentGridToolkit/BinaryExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentGridToolkit
+{
+    /// <summary>
+    /// Specifies the type of binary operation used to combine expressions.
+    /// </summary>
+    public enum BinaryExpression
+    {
+        /// <summary>
+        /// Logical AND operator, combining expressions with "&&".
+        /// </summary>
+        And,
+
+        /// <summary>
+        /// Short-circuiting logical AND operator, combining expressions with "&&" and stops evaluating once false is encountered.
+        /// </summary>
+        AndAlso,
+
+        /// <summary>
+        /// Logical OR operator, combining expressions with "||".
+        /// </summary>
+        Or,
+
+        /// <summary>
+        /// Short-circuiting logical OR operator, combining expressions with "||" and stops evaluating once true is encountered.
+        /// </summary>
+        OrElse
+    }
+}
---
+++ src/FluentGridToolkit/FilterExpression.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentGridToolkit
+{
+
+    /// <summary>
+    /// Represents a filter expression for dynamically building LINQ queries.
+    /// </summary>
+    /// <typeparam name="TProperty">The type of the property being filtered.</typeparam>
+    public class FilterExpression<TProperty>
+    {
+        /// <summary>
+        /// Gets or sets the name of the property to filter on.
+        /// </summary>
+        public string PropertyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the binary expression used to combine this filter with others.
+        /// </summary>
+        public BinaryExpression BinaryExpression { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value to compare against the property.
+        /// </summary>
+        /// <remarks>
+        /// The type of this value should match the type of the property being filtered.
+        /// </remarks>
+        public TProperty Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of a method to invoke on the property for evaluation.
+        /// </summary>
+        /// <remarks>
+        /// This is optional and is typically used for methods such as <see cref="string.Contains"/> or <see cref="string.StartsWith"/>.
+        /// If not set, default comparison operators will be used.
+        /// </remarks>
+        public string MethodName { get; set; }
+    }
+
+}
