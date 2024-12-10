a43880613c0fac928cbb6d745afda2e95da01cb7
Oscar Marin <oscar@marin.cr>
2024-12-09 21:28:36 -0600

    Added support for more complex filter expression

---
+++ src/FluentGridToolkit/FilterDataType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentGridToolkit
+{
+    /// <summary>
+    /// Indicates the underlying data type for a filter
+    /// </summary>
+    public enum FilterDataType
+    {
+        /// <summary>
+        /// Filter will be applied for numeric data of type system.double?
+        /// </summary>
+        Numeric,
+        /// <summary>
+        /// Filter will be applied for numeric data of type system.string?
+        /// </summary>
+        Text,
+        /// <summary>
+        /// Filter will be applied for numeric data of type system.DateTime?
+        /// </summary>
+        DateTime
+    }
+}
--- src/FluentGridToolkit/FilterValue.cs
+++ src/FluentGridToolkit/FilterValue.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +15,19 @@ namespace FluentGridToolkit
     public class FilterValue
     {
         /// <summary>
-        /// Gets or sets the value used for filtering.
+        /// Gets or sets the number value used for filtering.
         /// </summary>
-        public string? Value { get; set; }
+        public double? NumberValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the string value used for filtering.
+        /// </summary>
+        public string? StringValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DateTime value used for filtering.
+        /// </summary>
+        public DateTime? DateTimeValue { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the method to be invoked for filtering logic.
@@ -34,6 +45,52 @@ namespace FluentGridToolkit
         /// against the target property. The default is <see cref="FluentGridToolkit.ComparisonOperator.Equal"/>.
         /// </summary>
         public string? ComparisonOperator { get; set; } = nameof(FluentGridToolkit.ComparisonOperator.Equal);
-    }
 
+        /// <summary>
+        /// Converts to a <see cref="FilterExpression"/> object
+        /// </summary>
+        public FilterExpression ToFilterExpression(string propertyName, FilterDataType filterDataType)
+        {
+            object value = null;
+
+            switch (filterDataType)
+            {
+                case FilterDataType.Numeric:
+                    value = NumberValue;
+                    break;
+                case FilterDataType.DateTime:
+                    value = DateTimeValue;
+                    break;
+                default:
+                    value = StringValue;
+                    break;
+            }
+
+            return new FilterExpression()
+            {
+                BinaryExpression = BinaryExpression == "And" ? FluentGridToolkit.BinaryExpression.And : FluentGridToolkit.BinaryExpression.Or,
+                Operator = (ComparisonOperator)Enum.Parse(typeof(ComparisonOperator), ComparisonOperator),
+                PropertyName = propertyName,
+                Value = value
+            };
+        }
+
+        /// <summary>
+        /// Gets a value to indicate if the value of the class is null
+        /// </summary>
+        /// <param name="filterDataType">Data Type to evaluate</param>
+        public bool IsNullValue(FilterDataType filterDataType)
+        {
+            switch (filterDataType)
+            {
+                case FilterDataType.Numeric:
+                    return null == NumberValue;
+                case FilterDataType.DateTime:
+                    return null == DateTimeValue;
+                default:
+                    return null == StringValue;
+            }
+        }
+    }
 }
+
